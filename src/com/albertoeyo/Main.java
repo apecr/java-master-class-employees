7023e2eee7c1df4d911aff249544aad7c7e4e242
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-08-23 09:55:55 +0200

    first commit
    first commit
    
    [FA-49]

---
+++ src/com/albertoeyo/Main.java
@@ -0,0 +1,70 @@
+package com.albertoeyo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee john = new Employee("John Doe", 30);
+        Employee snow = new Employee("Snow White", 22);
+        Employee tim = new Employee("Tim Buckalka", 21);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee red = new Employee("Red RidingHood", 35);
+        Employee charming = new Employee("Prince Charming", 31);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+        employees.add(red);
+        employees.add(charming);
+
+
+        Function<Employee, String> getLastName = (Employee employee) ->
+                employee.getName().substring(employee.getName().indexOf(' ') + 1);
+
+        Function<Employee, String> getFirstName = employee ->
+                employee.getName().substring(0, employee.getName().indexOf(' '));
+
+        Random random1 = new Random();
+        employees.forEach(employee -> {
+            if (random1.nextBoolean()) {
+                System.out.println(getAName(getFirstName, employee));
+            } else {
+                System.out.println(getAName(getLastName, employee));
+            }
+        });
+
+        Function<Employee, String> upperCase = employee -> employee.getName().toUpperCase();
+        Function<String, String> firstName = name -> name.substring(0, name.indexOf(' '));
+        Function chainedFunction = upperCase.andThen(firstName);
+
+        System.out.println(chainedFunction.apply(employees.get(2)));
+
+        BiFunction<String, Employee, String> concatAge = (name, employee) -> name.concat(" " + employee.getAge());
+
+        System.out.println(concatAge.apply(upperCase.apply(employees.get(0)), employees.get(0)));
+
+
+    }
+
+    private static String getAName(Function<Employee, String> getName, Employee employee) {
+        return getName.apply(employee);
+    }
+
+    private static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> ageCondition) {
+        System.out.println("*********");
+        System.out.println(ageText);
+        for (Employee employee : employees) {
+            if (ageCondition.test(employee)) {
+                System.out.println(employee.getName());
+            }
+        }
+    }
+}
